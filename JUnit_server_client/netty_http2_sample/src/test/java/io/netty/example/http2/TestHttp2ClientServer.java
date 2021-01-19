810d85369a37a1555d1de08b081b34235b8d6482
zagood <ljbfif50@gmail.com>
2021-01-19 11:20:06 +0900

    JUnit Test

--- JUnit_server_client/netty_http2_sample/src/test/java/io/netty/example/http2/TestHttp2ClientServer.java
+++ JUnit_server_client/netty_http2_sample/src/test/java/io/netty/example/http2/TestHttp2ClientServer.java
@@ -2,9 +2,15 @@ package test.java.io.netty.example.http2;
 
 import org.junit.Test;
 
+import main.java.io.netty.example.http2.helloworld.client.Http2Client;
+import main.java.io.netty.example.http2.helloworld.server.Http2Server;
+
 public class TestHttp2ClientServer {
+	Http2Client client = new Http2Client();
+	Http2Server server = new Http2Server();
+	
 	@Test
-	public void testHttp2() throws Exception {
+	public void testCommunication() throws Exception {
 		
 	}
 }
